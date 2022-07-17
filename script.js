6905c1f376525a2f3f869da066d312c7ac95dd94
Robert <robertkoor0@gmail.com>
2022-07-17 21:31:24 +0300

    added operate function

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hi!")
+//basic operations 
+function add(a, b){
+    return parseFloat(a)+parseFloat(b);
+};
+
+function subtract(a, b){
+    return parseFloat(a)-parseFloat(b);
+};
+
+function multiply(a, b){
+    return parseFloat(a)*parseFloat(b);
+};
+
+function divide(a, b){
+    return Math.round(parseFloat(a)/parseFloat(b)*100)/100;
+};
+
+function operate(a, operation, b){
+    switch (operation!==""){
+        case operation == "+":
+            return add(a, b);
+            
+        case operation == "-":
+            return subtract(a, b);
+            
+        case operation == "*":
+            return multiply(a, b);
+            
+        case operation == "/":
+            return divide(a, b);
+            
+        default:
+            console.log("ERROR");
+    };
+};
