dae458d8317282c2cde894584d15b9cd0f53c1f4
Robert <robertkoor0@gmail.com>
2022-07-18 14:40:51 +0300

    Basic operations working

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Global variables
+const screen = document.querySelector('.nums');
+let operation = "";
+
+let isFloat = false;
+
 //basic operations 
 function add(a, b){
     return parseFloat(a)+parseFloat(b);
@@ -15,6 +21,7 @@ function divide(a, b){
     return Math.round(parseFloat(a)/parseFloat(b)*100)/100;
 };
 
+// Function which operates and returns answer
 function operate(a, operation, b){
     switch (operation!==""){
         case operation == "+":
@@ -30,6 +37,69 @@ function operate(a, operation, b){
             return divide(a, b);
             
         default:
-            console.log("ERROR");
+            return "ERROR";
     };
 };
+
+
+
+// Display the input to the screen
+function displayToScreen(e){
+    // stupid spaghetti code for not letting more than 1 comma into the number
+    if (!isFloat || (isFloat && e.target.id !==".")){
+        const textNode = document.createTextNode(e.target.id);
+        screen.appendChild(textNode);
+    } 
+    
+};
+
+
+
+// Get the input of numbers
+const input = document.querySelector('.numbers');
+input.addEventListener('click', function(e){
+    if (e.target.id==="."){
+        displayToScreen(e)
+        isFloat = true;
+    }
+    if (e.target.id !== "="){
+        displayToScreen(e)
+        //console.log(e.target.id);
+    } else if (e.target.id === "="){
+        let screenContent = screen.textContent.split(operation);
+        const answer = operate(screenContent[0], operation, screenContent[1])
+        screen.textContent = answer;
+        operation = ""
+        console.log(operation)
+    } 
+});
+
+
+
+// Clear screen functionality
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    screen.textContent = ""
+    isFloat = false;
+});
+
+
+
+
+// Change the operator + display it on screen
+function changeOperator(e){
+    operation = e.target.id;
+    const textNode = document.createTextNode(e.target.id);
+    screen.appendChild(textNode);
+
+}
+
+
+// Get the operation
+const operations = document.querySelector('.operators');
+operations.addEventListener('click', function(e){
+    if (operation===""){
+        changeOperator(e);
+        isFloat = false;
+    };
+});
